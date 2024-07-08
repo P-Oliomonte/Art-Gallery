a6fea67afcd9fd5c66c715945f29bb50b7aacff1
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-07-08 10:41:18 +0200

    Add style to Spotlight image

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -1,18 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Spotlight({ randomArtPiece }) {
   return (
     <>
       {/* <Link href={randomArtPiece.slug}> */}
-      <Image
-        src={randomArtPiece.imageSource}
-        alt={randomArtPiece.imageSource}
-        width={randomArtPiece.dimensions.width / 2}
-        height={randomArtPiece.dimensions.height / 2}
-      />
+      <ImageContainer>
+        <StyledImage
+          src={randomArtPiece.imageSource}
+          alt={randomArtPiece.imageSource}
+          layout="fill"
+        />
+      </ImageContainer>
       <h3>{randomArtPiece.artist}</h3>
       {/* </Link> */}
     </>
   );
 }
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 600px;
+`;
+
+const StyledImage = styled(Image)`
+  object-fit: cover;
+`;
